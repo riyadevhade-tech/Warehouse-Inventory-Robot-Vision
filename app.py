3ab945840cf96efa3dacb434560fd8fb9931634e
riyadevhade-tech <riyadevhade@gmail.com>
2026-09-08 16:12:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import sys
 from pathlib import Path
 
-PROJECT_ROOT = Path(__file__).resolve().parent.parent
+PROJECT_ROOT = Path(__file__).resolve().parent
 
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
@@ -12,7 +12,7 @@ if str(PROJECT_ROOT) not in sys.path:
     import sys
 from pathlib import Path
 
-PROJECT_ROOT = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 if str(PROJECT_ROOT) not in sys.path:
     sys.path.insert(0, str(PROJECT_ROOT))
